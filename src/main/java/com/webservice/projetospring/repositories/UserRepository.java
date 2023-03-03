46b943d31b778bf8d57a6e8b13a36a28d169ed6d
Gabriella <gabriellalok3@gmail.com>
2023-03-03 17:20:23 -0300

    implementando a camada de servico e registro de componentes

--- src/main/java/com/webservice/projetospring/repositories/UserRepository.java
+++ src/main/java/com/webservice/projetospring/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package com.webservice.projetospring.repositories;
 
 import com.webservice.projetospring.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
