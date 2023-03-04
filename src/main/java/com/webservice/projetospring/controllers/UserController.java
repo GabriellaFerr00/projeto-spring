1ddf01055ebd16c7dd9bf4cff6c1986c5f539e62
Gabriella <gabriellalok3@gmail.com>
2023-03-04 01:34:20 -0300

    implementando a camada de servico

--- src/main/java/com/webservice/projetospring/controllers/UserController.java
+++ src/main/java/com/webservice/projetospring/controllers/UserController.java
@@ -4,10 +4,12 @@ import com.webservice.projetospring.entities.User;
 import com.webservice.projetospring.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController //recurso web implementado por um controlador rest
 @RequestMapping(value = "/users")
