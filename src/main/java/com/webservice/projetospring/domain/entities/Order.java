d5ace32e11ec469d6c18cb85ef0c31e6f3035a4d
Gabriella <gabriellalok3@gmail.com>
2023-03-07 04:06:29 -0300

    implementando metodos SubTotal e Total

--- src/main/java/com/webservice/projetospring/domain/entities/Order.java
+++ src/main/java/com/webservice/projetospring/domain/entities/Order.java
@@ -61,6 +61,14 @@ public class Order {
         }
     }
 
+    public Double getTotal(){
+        double sum = 0.0;
+        for(OrderItem item : items){
+            sum += item.getSubTotal();
+        }
+        return sum;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
