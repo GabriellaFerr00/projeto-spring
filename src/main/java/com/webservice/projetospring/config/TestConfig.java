e94b0614522784e16ae3e6ecaafe5ccbcd7ab10f
Gabriella <gabriellalok3@gmail.com>
2023-03-07 00:16:15 -0300

    implementando classe product

--- src/main/java/com/webservice/projetospring/config/TestConfig.java
+++ src/main/java/com/webservice/projetospring/config/TestConfig.java
@@ -2,10 +2,12 @@ package com.webservice.projetospring.config;
 
 import com.webservice.projetospring.domain.entities.Category;
 import com.webservice.projetospring.domain.entities.Order;
+import com.webservice.projetospring.domain.entities.Product;
 import com.webservice.projetospring.domain.entities.User;
 import com.webservice.projetospring.domain.enums.OrderStatus;
 import com.webservice.projetospring.repositories.CategoryRepository;
 import com.webservice.projetospring.repositories.OrderRepository;
+import com.webservice.projetospring.repositories.ProductRepository;
 import com.webservice.projetospring.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -25,6 +27,8 @@ public class TestConfig implements CommandLineRunner {
     private OrderRepository orderRepository;
     @Autowired
     private CategoryRepository categoryRepository;
+    @Autowired
+    private ProductRepository productRepository;
 
 
     @Override
@@ -40,8 +44,15 @@ public class TestConfig implements CommandLineRunner {
         Category category2 = new Category(null, "Books");
         Category category3 = new Category(null, "Computers");
 
+        Product product1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+        Product product2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+        Product product3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+        Product product4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+        Product product5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+
         userRepository.saveAll(Arrays.asList(user1, user2));
         orderRepository.saveAll(Arrays.asList(order1, order2, order3));
         categoryRepository.saveAll(Arrays.asList(category1, category2, category3));
+        productRepository.saveAll(Arrays.asList(product1, product2, product3, product4, product5));
     }
 }
